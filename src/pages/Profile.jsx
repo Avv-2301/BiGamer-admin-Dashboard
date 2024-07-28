d98da1b13b3c5d0a304e92a199b7d9398c708149
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-28 20:59:32 +0530

    Profile-Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='flex flex-col justify-center items-center w-11/12 mt-1'>
+      <p className='font-bold text-[1.5rem] mb-4 bg-'>Admin Profile</p>
+      <div className='flex flex-col shadow-lg border lg:w-[35%] lg:h-[430px]'>
+
+      </div>
+    </div>
   )
 }
 
