6b3eff78a500c6c70e88eac6a20286cb669aed63
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-29 22:30:50 +0530

    Logout functionality

--- src/components/Bars/Sidebar.jsx
+++ src/components/Bars/Sidebar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { VscSignOut } from "react-icons/vsc";
 import { menus } from "../../data/routes";
 
 const Sidebar = () => {
+
+  const [confirmationModal, setConfirmationModal] = useState(null);
+  
+
   return (
     <section className="flex gap-6">
       <div
@@ -25,8 +30,21 @@ const Sidebar = () => {
             </Link>
           ))}
           <button
-          
-          />
+          onClick={() =>setConfirmationModal({
+                text1: "Are you sure?",
+                text2: "You will be logged out of your account.",
+                btn1Text: "Logout",
+                btn2Text: "Cancel",
+                // btn1Handler: () => dispatch(logout(navigate)),
+                btn2Handler: () => setConfirmationModal(null),
+          })      
+        }
+        className="px-2 py-2 text-sm font-medium text-black hover:bg-[#dcfce7] rounded-md drop-shadow-lg text-[#111827]">
+           <div className="flex items-center gap-x-2">
+              <VscSignOut className="text-lg" />
+              <span>Logout</span>
+            </div>
+            </button>
         </div>
       </div>
     </section>
